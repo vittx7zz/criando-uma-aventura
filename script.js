745b39aaef7519387390460741ca85636591006f
vittx7zz <vitor.rodrigues15@escola.pr.gov.br>
2025-10-24 11:47:05 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const avanca = ducument.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function () {
-        const atual = document.querySelector('.ativo')
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classlist.remove('ativo');
-        document.getElementById(proximoPasso).classlist.ad('ativo')
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
-    ;
